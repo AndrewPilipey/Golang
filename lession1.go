907f769a957a13759e991c4cf2a64b2700e76dab
https://github.com/AndrewPilipey <andrewpilipeyone@gmail.com>
2023-01-09 22:58:32 +0300

    forming a habit

---
+++ lession1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+	fmt.Println("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Println("Enter your age: ")
+	fmt.Scan(&age)
+
+	fmt.Println("Your name is:", name)
+	fmt.Println("Your age is:", age)
+}
